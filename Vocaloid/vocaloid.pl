df55882b65c54662464d561395fef7c1c72dcfb3
Maqueda Paul <maquedapablodavid@gmail.com>
2021-08-27 19:50:36 -0300

    Base de conocimientos y 1) [Vocaloid]

--- Vocaloid/vocaloid.pl
+++ Vocaloid/vocaloid.pl
@@ -1,6 +1,26 @@
 
 
+canta(megurineLuka, cancion(nightFever,4)).
+canta(megurineLuka, cancion(foreverYoung,5)).
+
+canta(hatsuneMiku, cancion(tellYourWorld,4)).
+
+canta(gumi, cancion(foreverYoung,4)).
+canta(gumi, cancion(tellYourWorld,5)).
+
+canta(seeU, cancion(novemberRain,6)).
+canta(seeU, cancion(nightFever,5)).
 
 
+esNovedoso(Vocaloid):-
+    cantaAlMenosDos(Vocaloid),
+    findall(Duracion, canta(Vocaloid, cancion(_,Duracion)), Duraciones),
+    Duraciones < 15.
 
+duracion(cancion(_,Duracion), Duracion).
+
+cantaAlMenosDos(Vocaloid):-
+    canta(Vocaloid, Cancion),
+    canta(Vocaloid, OtraCancion),
+    Cancion \= OtraCancion.
 
