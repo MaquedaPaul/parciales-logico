853f99c46dcdb8f737bd613aa874b87177535c16
Maqueda Paul <maquedapablodavid@gmail.com>
2021-08-29 20:26:46 -0300

    Punto 2 Resuelto [AmigoInvisible]

--- AmigoInvisible/AmigoInvisible.pl
+++ AmigoInvisible/AmigoInvisible.pl
@@ -1,5 +1,5 @@
 %nacio(Fecha, Persona).
-nacio(fecha(9, 7, 1994), juan).
+nacio(fecha(9, 9, 1994), juan).
 nacio(fecha(26, 3, 1992), aye).
 nacio(fecha(22, 12, 1995), feche).
 
@@ -30,17 +30,30 @@ yaPaso(Persona):-
 
 %regalo(Regalador, Regalado, Regalo, Fecha)-
 
-leRegalo(regalo(juan, feche, libroTP), 2018).
-leRegalo(regalo(juan, aye, productoHP), 2019).
-leRegalo(regalo(juan, aye, cervezaRoja), 2020).
-leRegalo(regalo(juan, feche, libroTP), 2021).
+leRegalo(juan, feche, libroTP, 2018).
+leRegalo(juan, aye, productoHP, 2019).
+leRegalo(juan, aye, cervezaRoja, 2020).
+leRegalo(juan, feche, libroTP, 2021).
 
-leRegalo(regalo(aye, feche, libroTP), 2019).
-leRegalo(regalo(aye, juan, libroSL), 2020).
+leRegalo(aye, feche, libroTP, 2019).
+leRegalo(aye, juan, libroSL, 2020).
 
-leRegalo(regalo(feche, juan, cervezaRubia), 2019).
-leRegalo(regalo(feche, juan, productoP), 2020).
-leRegalo(regalo(feche, aye, libroMS), 2021).
+leRegalo(feche, juan, cervezaRubia, 2019).
+leRegalo(feche, juan, productoP, 2020).
+leRegalo(feche, aye, libroMS, 2021).
+
+podriaRegalarle(OtraPersona, Persona, Fecha):-
+    nacio(_,OtraPersona),
+    nacio(_,Persona),
+    OtraPersona \= Persona,
+    not(leRegalo(Persona, _, _, Fecha)),
+    not(yaPaso(OtraPersona)),
+    not(recibioRegaloEn(OtraPersona, Fecha)).
+
+
+recibioRegaloEn(Persona, Fecha):-
+    leRegalo(_,Persona,_,Fecha).
+
 
 
 
