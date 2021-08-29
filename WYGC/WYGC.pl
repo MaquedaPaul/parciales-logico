14445ba1b5f569ebd69fb44251ab1659e915330a
Maqueda Paul <maquedapablodavid@gmail.com>
2021-08-29 14:13:06 -0300

    Punto 5 y Correciones [WYGC]

--- WYGC/WYGC.pl
+++ WYGC/WYGC.pl
@@ -29,7 +29,7 @@ puedeRealizar(Integrante, Tarea):-
     tiene(Integrante,_),
     herramientasRequeridas(Tarea, Herramientas),
     forall(member(Herramienta, Herramientas)
-    , satisfaceNecesidad(Integrante, herramienta)).
+    , satisfaceNecesidad(Integrante, Herramienta)).
 
 puedeRealizar(Integrante, Tarea):-
     herramientasRequeridas(Tarea,_),
@@ -38,27 +38,64 @@ puedeRealizar(Integrante, Tarea):-
 %Punto 4
 
 tareaPedida/3.
-%tareaPedida(Cliente, TareaPedida, MetrosCuadrados).
+%tareaPedida(TareaPedida, Cliente, MetrosCuadrados).
 precio/2.
 %precio(Tarea, PrecioPorMetroCuadrado).
 
 
 
 seLeCobra(Cliente, Cobro):-
-    tareaPedida(Cliente,_,_),
-    findall(Precio, (tareaPedida(Cliente, Tarea, Metros), precio(Tarea, PrecioPorTarea), Precio is PrecioPorTarea * Metros), Cobro).
+    tareaPedida(_, Cliente,_),
+    findall(Precio, calculoDelPrecio(Cliente, Precio, _), Precios),
+    sum_list(Precios, Cobro).
+
+calculoDelPrecio(Cliente, Precio, Tarea):-
+    tareaPedida(Tarea, Cliente, Metros),
+    precio(Tarea, PrecioPorTarea),
+    Precio is PrecioPorTarea * Metros.
 
 %Punto 5
+tareaPedida(ordenarCuarto, dana, 20).
+tareaPedida(cortarPasto, walter, 50).
+tareaPedida(limpiarTecho, walter, 70).
+tareaPedida(limpiarBanio, louis, 15).
+
+precio(ordenarCuarto, 13).
+precio(limpiarTecho, 20).
+precio(limpiarBanio, 55).
+precio(cortarPasto, 10).
+precio(encerarPisos, 7).
+
+
 
 aceptaria(Integrante, Cliente):-
     tiene(Integrante, _),
-    tareaPedida(Cliente,_,_),
-    forall(tareaPedida(Cliente, Tarea, _), (puedeRealizar(Integrante, Tarea), estaDispuesto(Tarea, Integrante))).
+    tareaPedida(_,Cliente,_),
+    forall(tareaPedida(Tarea, Cliente, _), (puedeRealizar(Integrante, Tarea), estaDispuesto(Tarea, Integrante))).
 
-estaDispuesto(Tarea, ray):-
-    not(herramientasRequeridas(limpiarTecho,_)).
+estaDispuesto(Pedido, ray):-
+    tareaPedida(Pedido,_,_),
+    Pedido \= limpiarTecho.
+
+estaDispuesto(_, winston):-
+    seLeCobra(_, Cobro),
+    Cobro > 500.
 
 
+estaDispuesto(Pedido, egon):-
+    tareaPedida(Pedido,_,_),
+    not(tareaCompleja(Pedido)).
+
+estaDispuesto(Pedido, peter):-
+    tareaPedida(Pedido,_,_).
+
+
+
+tareaCompleja(Tarea):-
+    herramientasRequeridas(Tarea, Herramientas),
+    length(Herramientas, Cantidad),
+    Cantidad > 2.
+    
 
 
 %Punto 6
