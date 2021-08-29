5f82c9e984f3ad07387a1c397578cb9f2c7c3788
Maqueda Paul <maquedapablodavid@gmail.com>
2021-08-29 00:48:50 -0300

    Punto 1 Resuelto [WYGC]

--- WYGC/WYGC.pl
+++ WYGC/WYGC.pl
@@ -1,5 +1,16 @@
 
+herramientasRequeridas(ordenarCuarto, [aspiradora(100), trapeador, plumero]).
+herramientasRequeridas(limpiarTecho, [escoba, pala]).
+herramientasRequeridas(cortarPasto, [bordedadora]).
+herramientasRequeridas(limpiarBanio, [sopapa, trapeador]).
+herramientasRequeridas(encerarPisos, [lustradpesora, cera, aspiradora(300)]).
 
+tiene(egon, herramienta(aspiradora(200))).
+
+tiene(egon, herramienta(trapeador)).
+tiene(peter, herramienta(trapeador)).
+
+tiene(winston, herramienta(varitaDeNeutrones)).
 
 
 
